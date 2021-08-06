cfa64188427c84ae66a4bb74504917ad90fc6b96
Rohit08769 <59139153+Rohit08769@users.noreply.github.com>
2021-08-06 12:44:18 +0530

    Create problem_2.py

---
+++ Week 3/problem_2.py
@@ -0,0 +1,20 @@
+def selection_sort(l1):
+    swaps, comparison = 0, 0
+    for i in range(0,len(l1)-1):
+        min = i
+        for j in range(i+1, len(l1)):
+            comparison+=1
+            if l1[j] < l1[min]:
+                min = j
+
+        l1[i],l1[min] = l1[min], l1[i]
+        swaps+=1
+    print(f"{' '.join(list(map(str,l1)))} \ncomparision = {comparison} \nswaps = {swaps}")
+
+
+
+test_case= int(input())
+for i in range(test_case):
+    test_ele= int(input())
+    l1=list(map(int,input().split()))
+    selection_sort(l1)
